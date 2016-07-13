85e36a598da0869f4cbd7eca63a31e636ee3aa42
Rifat Rashid <s-RRashid@lwsd.org>
2016-07-13 12:44:25 -0700

    changed MainActivity to extend Activity

--- app/src/main/java/com/example/seize/cpuoverlay/MainActivity.java
+++ app/src/main/java/com/example/seize/cpuoverlay/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.seize.cpuoverlay;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import wei.mark.standout.StandOutWindow;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        StandOutWindow.closeAll(this, FloatingWindow.class);
+        StandOutWindow.show(this, FloatingWindow.class, StandOutWindow.DEFAULT_ID);
+        finish();
     }
 }
